b214f687e5674c611d545bdd634a17457a7da64d
Fredy Laksmono <fredy.laksmono@gmail.com>
2022-11-21 16:43:20 -0600

    Add: course page api call for add course

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,13 +1,41 @@
 import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { updateAllCoursesList } from "../actions";
-import { GetCourseList } from "../services/CourseServices";
+import { AddCourse, GetCourseList } from "../services/CourseServices";
 import CourseCard from "../components/CourseCard";
 
 const Courses = () => {
   const [needRefresh, setNeedRefresh] = useState([true]);
+  const [courseAddMode, setCourseAddMode] = useState(false);
   const dispatch = useDispatch();
   const allCoursesList = useSelector((state) => state.allCoursesList);
+  const [formValues, setFormValues] = useState({
+    name: "",
+    description: ""
+  });
+
+  const enableAddCourseMode = () => {
+    setCourseAddMode(true);
+  };
+
+  const disableAddCourseMode = () => {
+    setCourseAddMode(false);
+  };
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    let createdCourse = await AddCourse(formValues);
+    disableAddCourseMode();
+    setFormValues({
+      name: "",
+      description: ""
+    });
+    setNeedRefresh(true);
+  };
 
   useEffect(() => {
     let getCoursesList = async () => {
@@ -21,6 +49,39 @@ const Courses = () => {
     }
   }, [needRefresh]);
 
+  let addButtonRender = <button disabled>Add Course</button>;
+
+  if (formValues.name && formValues.description) {
+    addButtonRender = <button type="submit">Add Course</button>;
+  }
+
+  let courseAddRender = (
+    <button onClick={enableAddCourseMode}>Add Course</button>
+  );
+  if (courseAddMode) {
+    courseAddRender = (
+      <form onSubmit={handleSubmit}>
+        <label>Course name</label>
+        <input
+          onChange={handleChange}
+          value={formValues.name}
+          name="name"
+          type="text"
+          placeholder="Astrology"
+        />
+        <label>Description</label>
+        <input
+          onChange={handleChange}
+          value={formValues.description}
+          name="description"
+          type="text"
+          placeholder="Teaching about space universe"
+        />
+        {addButtonRender}
+      </form>
+    );
+  }
+
   let courseListRender = (
     <div>
       {allCoursesList.map((course) => (
@@ -32,6 +93,7 @@ const Courses = () => {
   let toRender = (
     <div>
       <div>this is courses page</div>
+      <div>{courseAddRender}</div>
       <div>{courseListRender}</div>
     </div>
   );
--- src/services/CourseServices.js
+++ src/services/CourseServices.js
@@ -1,34 +1,19 @@
 import Client from "./api";
 import axios from "axios";
 
+export const AddCourse = async (data) => {
+  try {
+    let response = await axios.post("http://localhost:3001/api/courses/", data);
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const GetCourseList = async () => {
   try {
-    let response = await axios.get("http://localhost:3001/api/course/");
+    let response = await axios.get("http://localhost:3001/api/courses/");
     return response.data;
-
-    // let tempData = [
-    //   {
-    //     id: 1,
-    //     name: "Mathematic",
-    //     description: "Mathematic course "
-    //   },
-    //   {
-    //     id: 2,
-    //     name: "English",
-    //     description: "English course"
-    //   },
-    //   {
-    //     id: 3,
-    //     name: "Science",
-    //     description: "Science course"
-    //   },
-    //   {
-    //     id: 4,
-    //     name: "Sport",
-    //     description: "Sport course"
-    //   }
-    // ];
-    // return tempData;
   } catch (error) {
     throw error;
   }
