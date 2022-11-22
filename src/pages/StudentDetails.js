8049d835db80de7aad6c5df8770e1acdad7f8f8e
Jyotir McEwanAnamoah <jyotirjy@gmail.com>
2022-11-22 18:18:09 +0000

    new student and relaqtion card styles

--- src/pages/StudentDetails.js
+++ src/pages/StudentDetails.js
@@ -1,60 +1,61 @@
-import { useEffect, useState } from "react";
-import { useNavigate, useParams } from "react-router-dom";
-import { useSelector } from "react-redux";
-import CourseSelectCard from "../components/CourseSelectCard";
-import GradeSelectCard from "../components/GradeSelectCard";
-import { AddRelation, GetStudentRelation } from "../services/RelationServices";
-import RelationCard from "../components/RelationCard";
+import { useEffect, useState } from 'react'
+import { useNavigate, useParams } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import CourseSelectCard from '../components/CourseSelectCard'
+import GradeSelectCard from '../components/GradeSelectCard'
+import { AddRelation, GetStudentRelation } from '../services/RelationServices'
+import RelationCard from '../components/RelationCard'
+import '../styles/RelationsCard.css'
 
 const StudentDetails = () => {
-  const { studentId, studentName } = useParams();
-  const allCoursesList = useSelector((state) => state.allCoursesList);
-  const allGradesList = useSelector((state) => state.allGradesList);
+  const { studentId, studentName } = useParams()
+  const allCoursesList = useSelector((state) => state.allCoursesList)
+  const allGradesList = useSelector((state) => state.allGradesList)
 
-  const [needRefresh, setNeedRefresh] = useState([true]);
-  const [relationList, setRelationList] = useState([]);
+  const [needRefresh, setNeedRefresh] = useState([true])
+  const [relationList, setRelationList] = useState([])
   const [formValues, setFormValues] = useState({
     studentId: studentId,
     courseId: allCoursesList[0].id.toString(),
     gradeId: allGradesList[0].id.toString()
-  });
+  })
 
   const handleChange = (e) => {
-    setFormValues({ ...formValues, [e.target.id]: e.target.value });
-  };
+    setFormValues({ ...formValues, [e.target.id]: e.target.value })
+  }
 
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    let addRelation = await AddRelation(formValues);
+    e.preventDefault()
+    let addRelation = await AddRelation(formValues)
     setFormValues({
       studentId: studentId,
       courseId: allCoursesList[0].id.toString(),
       gradeId: allGradesList[0].id.toString()
-    });
-    setNeedRefresh(true);
-  };
+    })
+    setNeedRefresh(true)
+  }
 
   useEffect(() => {
     const getAllRelations = async () => {
-      let data = await GetStudentRelation(studentId);
-      setRelationList(data);
-      setNeedRefresh(false);
-    };
+      let data = await GetStudentRelation(studentId)
+      setRelationList(data)
+      setNeedRefresh(false)
+    }
 
-    getAllRelations();
-  }, [needRefresh]);
+    getAllRelations()
+  }, [needRefresh])
 
-  console.log("myList", relationList);
+  console.log('myList', relationList)
   let relationListRender = (
     <div>
       {relationList.map((relation) => (
         <RelationCard key={relation.id} relation={relation} />
       ))}
     </div>
-  );
+  )
 
   let addStudentCourseRender = (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={handleSubmit} className="course-form">
       <label>Course</label>
       <select id="courseId" onChange={handleChange}>
         {allCoursesList.map((course) => (
@@ -69,15 +70,15 @@ const StudentDetails = () => {
       </select>
       <button type="submit">Add</button>
     </form>
-  );
+  )
   let toRender = (
     <div>
-      <div>{studentName}</div>
+      <h1>{studentName}</h1>
       {relationListRender}
       {addStudentCourseRender}
     </div>
-  );
+  )
 
-  return toRender;
-};
-export default StudentDetails;
+  return toRender
+}
+export default StudentDetails
