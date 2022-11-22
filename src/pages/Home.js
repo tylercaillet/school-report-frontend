c8d4fb44b6cac0847d6609841f0db9b07c5f8d60
Tyler <tylercaillet2121@gmail.com>
2022-11-22 13:35:34 -0500

    not going to use

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,7 @@
 import "../styles/Home.css";
 import schoolimg from "../assets/schoolimg.jpg";
-import { useDispatch } from "react-redux";
-import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 import {
   updateAllCoursesList,
   updateAllStudentsList,
@@ -10,9 +10,35 @@ import {
 import { GetCourseList } from "../services/CourseServices";
 import { GetStudentList } from "../services/StudentServices";
 import { GetGrades } from "../services/GradeServices";
+import StudentCard from '../components/StudentCard'
+import { Navigate, useNavigate } from "react-router-dom";
 
 const Home = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
+  const allStudentsList = useSelector((state) => state.allStudentsList)
+  const [formValues, setFormValues] = useState({
+    name: ''
+  })
+  // const [selectedStudent, setSelectedStudent] = useState([])
+  const [needRefresh, setNeedRefresh] = useState([true])
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    let student = await GetStudentList(formValues)
+    setFormValues({
+      name: ''
+    })
+    navigate(`/students`)
+  }
+
+  // const chooseStudent = (selected) => {
+  //   setSelectedStudent(selected)
+  //   navigate(`/${selected.id}`)
+  // }
 
   useEffect(() => {
     const getCoursesList = async () => {
@@ -33,8 +59,31 @@ const Home = () => {
   }, []);
 
   let someStatisticRender = <div>Some useful info to show</div>;
+  let searchStudentRender = (
+    <div className="search-container-home">
+      <div className="search-container-box">
+      <form onSubmit={handleSubmit}  className="search-student-form">
+        <input
+          onChange={handleChange}
+          value={formValues.name}
+          name="name"
+          type="text"
+          placeholder="Search here"
+        />
+        <button type="submit" className="search-student">
+          Search
+        </button>
+      </form>
+      </div>
+    </div>
+  )
+ 
+  
+
+
   let toRender = (
     <div className="home-container">
+      {searchStudentRender}
       <div className="col-1">
         <h1>Browse Student & Courses Available</h1>
         {someStatisticRender}
@@ -44,6 +93,7 @@ const Home = () => {
       </div>
     </div>
   );
+  
   return toRender;
 };
 
